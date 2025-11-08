608e3ae5fdcfa0881cec3ccb1419b770bdff7626
Pavel Liakhouski <pavelljahovskij97@gmail.com>
2025-11-08 17:45:40 +0100

    feat: just add agent handler
    Signed-off-by: Pavel Liakhouski <pavelljahovskij97@gmail.com>

---
+++ src/backend/handlers/agent_chat_handler.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from langchain_core.messages import AIMessage
+from starlette.websockets import WebSocket
+
+from agents.graph import GRAPH_MEMORY, MCP_SERVER_CLIENT, create_graph
+from configs.logger import LOGGER
+from exceptions.agent_exceptions import GeneralAgentException
+
+from schemas.websocket import UserRequest, BotResponse
+
+
+class AgentChatHandler:
+    def __init__(self, user_request: UserRequest, websocket: WebSocket):
+        self.user_request = user_request
+        self.websocket = websocket
+
+    async def handle_request(self) -> Optional[BotResponse]:
+        config = {
+            "configurable": {
+                "thread_id": self.user_request.session_id
+            },
+            "checkpoint": GRAPH_MEMORY,
+        }
+        last_message = None
+        try:
+            async with MCP_SERVER_CLIENT.session("gov") as gov_session:
+                agent = await create_graph(gov_session)
+                async for name, mode, chunk in agent.astream(
+                        {"messages": self.user_request.text},
+                        subgraphs=True,
+                        config=config,
+                        stream_mode=["custom", "values"]
+                ):
+                    if mode == "custom":
+                        pass
+                    elif mode == "values":
+                        last_message = next(iter(chunk.values()))
+                    else:
+                        LOGGER.error(f"Unexpected mode: {mode}")
+                if isinstance(last_message[-1], AIMessage):
+                    return BotResponse(text=last_message[-1].content)
+        except Exception as ex:
+            LOGGER.error(f"Error in AgentChatHandler: {ex}")
+            raise GeneralAgentException(ex)
